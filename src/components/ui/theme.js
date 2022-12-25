03ad1c6f2755b388fc1e6b2b67b06dbf2e55173c
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-12-25 23:57:59 +0330

    - customize default theme for change style in components

--- src/components/ui/theme.js
+++ src/components/ui/theme.js
@@ -1,4 +1,5 @@
 import {createTheme} from "@mui/material";
+import {blue, red} from "@mui/material/colors";
 
 //NOTE create custom theme
 export const theme = createTheme({
@@ -14,5 +15,30 @@ export const theme = createTheme({
     },
     typography: {
         fontFamily: "vazir, roboto",
-    }
+    },
+    components: {
+        MuiButtonBase: {
+            defaultProps: {
+                disableRipple: false,
+            }
+        },
+        MuiButton: {
+            variants: [
+                {
+                    props: { variant: 'dashed' },
+                    style: {
+                        textTransform: 'none',
+                        border: `2px dashed ${blue[500]}`,
+                    },
+                },
+                {
+                    props: { variant: 'dashed', color: 'secondary' },
+                    style: {
+                        border: `4px dashed ${red[500]}`,
+                    },
+                },
+            ],
+        },
+    },
+
 });
