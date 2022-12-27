02a5ca4ce059f8d7d809c5a369d0fb309ddcc5e5
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-12-27 22:36:00 +0330

    - Add grid2 of MaterialUI at MainLayout

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,10 +1,11 @@
-import {ThemeProvider} from "@mui/material";
+import {ThemeProvider, Typography} from "@mui/material";
 import {theme} from "../ui/theme";
 import {Helmet, HelmetProvider} from "react-helmet-async";
 import {CacheProvider} from "@emotion/react";
 import createCache from "@emotion/cache";
 import {prefixer} from "stylis";
 import rtlPlugin from "stylis-plugin-rtl";
+import Grid from "@mui/material/Unstable_Grid2";
 
 //NOTE Create RTL cache
 const cacheRTL = createCache({
@@ -20,6 +21,15 @@ export const MainLayout = ({children}) => {
                     <Helmet>
                         <title>Mustafa Saeidi's WebSite</title>
                     </Helmet>
+                    {/*Grid System*/}
+                    <Grid container sx={{height: "100vh"}}>
+                        <Grid xs={0} sm={0} md={3} lg={3} xl={2} sx={{backgroundColor: "primary.main"}}>
+                            <Typography variant="h6">Sidebar</Typography>
+                        </Grid>
+                        <Grid xs={12} sm={12} md={9} lg={9} xl={10} sx={{backgroundColor: "secondary.main"}}>
+                            <Typography variant="h6" sx={{textAlign: "center"}}>Main Content</Typography>
+                        </Grid>
+                    </Grid>
                     {children}
                 </HelmetProvider>
             </ThemeProvider>
